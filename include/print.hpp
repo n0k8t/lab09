4c8c4483d3198ffea9aa442003240201750377fb
Timofey Tyrin <timat-ti@mail.ru>
2017-10-19 22:27:02 +0300

    Update print.hpp

--- include/print.hpp
+++ include/print.hpp
@@ -2,5 +2,5 @@
 #include <fstream>
 #include <iostream>
 
-void print(const std::string& text, std::ostream& out = std::cout);
-void print(const std::string& text, std::ofstream& out);
+void print(const std::string& text, std::ostream& out = std::cout); // вывод строки в стандартный поток std::cout
+void print(const std::string& text, std::ofstream& out);  // вывод в файловый поток
